69b4b2114697191062372ef814a7c65024af53dc
Derek Shoemaker <shoemakerdr@gmail.com>
2017-04-04 16:34:57 +0000

    Completed url concatenator variable, and started api function.

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 var weather = (function () {
-    var location = {
-        lat: "lat",
-        lon: "long"
-    }
+  var location = {
+    lat: "lat",
+    lon: "long"
+  };
   
-    var api = function() {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                var url = "api.openweathermap.org/data/2.5/weather?lat=" +
-                           location.lat + "&lon=" + location.lon +
-                           "&APPID=058e4fada3e29a7594f7a287dcad121d"
-            }
-        }
-    };
-  
-    return {
-        render: function() {
-        
-        },
-        
-        toF: function() {
-          
-        },
-    
-        toC: function() {
-          
-        }
+  var url = "http://samples.openweathermap.org/data/2.5/weather?lat=" + 
+            location.lat + "&lon=" + location.lon + 
+            "&APPID=058e4fada3e29a7594f7a287dcad121d";
+
+  var api = function() {
+    var req = new XMLHttpRequest();
+    req.open("GET", url, false);
+    req.send(null);
+    req.responseText;
+  };
+
+  return {
+    render: function() {
+
+    },
+
+    toF: function() {
+
+    },
+
+    toC: function() {
+
     }
-}());
+  };
+})();

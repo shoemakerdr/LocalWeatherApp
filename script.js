dc957077cb05205227b8a8b573e0e8a0405fc5ee
Derek Shoemaker <shoemakerdr@gmail.com>
2017-04-21 14:34:11 +0000

    added to weather module. still having issues with async calls

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-var weather = (function () {
-
-  navigator.geolocation.getCurrentPosition(success);
-
-  function success(position) {
-    var url = "http://api.openweathermap.org/data/2.5/weather?lat=" +
-              position.coords.latitude + "&lon=" + position.coords.latitude +
-              "&APPID=058e4fada3e29a7594f7a287dcad121d";
-
-    var api = function() {
-      var req = new XMLHttpRequest();
-      req.open("GET", url, false);
-      req.send(null);
-      return JSON.parse(req.responseText);
-    };
-
+let weather = (function () {
+  let coord = {
+	  lat: 0,
+	  long: 0
+  };
+  
+  let url = "http://api.openweathermap.org/data/2.5/weather?lat=" + 
+            coord.lat + "&lon=" + coord.long + 
+            "&APPID=058e4fada3e29a7594f7a287dcad121d";
+  
+  const getCoords = function() {
+    navigator.geolocation.getCurrentPosition(success);
+  
+    function success(position) {
+      coord.lat = position.coords.latitude;
+      coord.long = position.coords.longitude;
+    }
   }
+  
+  const api = (function() {
+    getCoords();
+    const req = new XMLHttpRequest();
+    req.open("GET", url, false);
+    req.send(null);
+    return JSON.parse(req.responseText);
+  })();
 
   return {
     render: function() {
